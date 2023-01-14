215400a73bd39531d2e75b6a4b68a9d5db92ee34
Evan Simanovskis <evan.simanovskis@gmail.com>
2023-01-14 17:34:17 -0500

    added model training code

--- src/scripts/train_model.py
+++ src/scripts/train_model.py
@@ -1,10 +1,15 @@
-from config import DATA_DIR
-from preprocessing.image_loader import load_data
+import tensorflow as tf
+
+from config import (BATCH_SIZE, DATA_DIR, EPOCHS, MODEL_INPUT_SHAPE,
+                    MODEL_PATH, VALIDATION_SPLIT)
+from model.convolutional_neural_network import build_model
 
 
 def train_model():
-    dataset = load_data(DATA_DIR)
+    training_dataset, testing_dataset = tf.keras.preprocessing.image_dataset_from_directory(DATA_DIR, image_size=MODEL_INPUT_SHAPE, batch_size=BATCH_SIZE, validation_split=VALIDATION_SPLIT, subset="both", seed=0)
 
-    for data, labels in dataset.take(1):
-        print(data.shape)
-        print(labels.shape)
+    model = build_model((*MODEL_INPUT_SHAPE, 3))
+    model.compile('adam', 'binary_crossentropy')
+
+    model.fit(training_dataset, epochs=EPOCHS, validation_data=testing_dataset)
+    model.save_weights(MODEL_PATH)
