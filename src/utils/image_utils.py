0ed087b8afd6b7c6c72f6612acc624f42e5c03ad
Evan Simanovskis <evan.simanovskis@gmail.com>
2023-01-15 15:25:35 -0500

    fixed

--- src/utils/image_utils.py
+++ src/utils/image_utils.py
@@ -6,6 +6,7 @@ from config import MODEL_INPUT_SHAPE
 # Loads an image file, then resizes it to the correct size for the model.
 def load_image(filepath):
     im = Image.open(filepath)
+    im = im.convert("RGB")
     im = im.resize(reversed(MODEL_INPUT_SHAPE)) # This has to be reversed as the model input shape is (height, width)
     im = np.asarray(im)
 
