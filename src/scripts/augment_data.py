cd357ae8059eee1f189146cf24293e52a0b1cdaf
Evan Simanovskis <evan.simanovskis@gmail.com>
2023-01-15 13:13:32 -0500

    added code to augment the fake food tickets

--- src/scripts/augment_data.py
+++ src/scripts/augment_data.py
@@ -2,13 +2,23 @@ from config import (
     N_AUGMENTATIONS,
     REAL_FOOD_TICKET_PATH_DATASET,
     REAL_FOOD_TICKET_PATH_PREPROCESSED,
+    FAKE_FOOD_TICKET_PATH_DATASET,
+    FAKE_FOOD_TICKET_PATH_PREPROCESSED,
 )
 from preprocessing.data_augmentation import data_augmentation
 
 
 def augment_data():
+    # Augment the real food tickets.
     data_augmentation(
         REAL_FOOD_TICKET_PATH_DATASET,
         REAL_FOOD_TICKET_PATH_PREPROCESSED,
+        N_AUGMENTATIONS,
+    )
+
+    # Augment the fake food tickets.
+    data_augmentation(
+        FAKE_FOOD_TICKET_PATH_DATASET,
+        FAKE_FOOD_TICKET_PATH_PREPROCESSED,
         N_AUGMENTATIONS,
     )
