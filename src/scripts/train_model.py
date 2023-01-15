1075647d11b5c5453791eda43cd6cb3954046920
Evan Simanovskis <evan.simanovskis@gmail.com>
2023-01-14 21:06:51 -0500

    added tensorboard support

--- src/scripts/train_model.py
+++ src/scripts/train_model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-from config import (BATCH_SIZE, DATA_DIR, EPOCHS, MODEL_INPUT_SHAPE,
+from config import (BATCH_SIZE, DATA_DIR, EPOCHS, LOG_DIR, MODEL_INPUT_SHAPE,
                     MODEL_PATH, VALIDATION_SPLIT)
 from model.convolutional_neural_network import build_model
 
@@ -9,7 +9,9 @@ def train_model():
     training_dataset, testing_dataset = tf.keras.preprocessing.image_dataset_from_directory(DATA_DIR, image_size=MODEL_INPUT_SHAPE, batch_size=BATCH_SIZE, validation_split=VALIDATION_SPLIT, subset="both", seed=0)
 
     model = build_model((*MODEL_INPUT_SHAPE, 3))
-    model.compile('adam', 'binary_crossentropy')
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=["accuracy"])
 
-    model.fit(training_dataset, epochs=EPOCHS, validation_data=testing_dataset)
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=LOG_DIR)
+
+    model.fit(training_dataset, epochs=EPOCHS, validation_data=testing_dataset, callbacks=[tensorboard_callback])
     model.save_weights(MODEL_PATH)
